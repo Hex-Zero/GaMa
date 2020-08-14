6497dceae3387f257bbfa3327f329623ca3db1a5
Hex-Zero <hex0@live.com>
2020-08-14 08:31:47 +0100

    Add buttons and add gms style

--- GameManagerUi/Startup.cs
+++ GameManagerUi/Startup.cs
@@ -22,7 +22,6 @@ namespace GameManagerUi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<GaMaDbContext>(options =>
@@ -33,7 +32,6 @@ namespace GameManagerUi
             services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -43,7 +41,6 @@ namespace GameManagerUi
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
--- GameManagerUi/wwwroot/js/site.js
+++ GameManagerUi/wwwroot/js/site.js
@@ -1,6 +1,1 @@
-﻿const navBar = $("body")
-
-navBar.on("click", function (e) {
-    console.log(e)
-    $(e.target).append($(document.createElement("div")).html("hi"))
-})
+﻿
